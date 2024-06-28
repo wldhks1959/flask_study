7ef0036bdde22eac820aef660de0db81dc58ad35
wldhks1959 <gg812056@gmail.com>
2024-06-28 22:40:25 +0900

    second

--- BUILD/application.py
+++ BUILD/application.py
@@ -6,5 +6,14 @@ application = Flask(__name__)
 def hello():
     return render_template("hello.html")
 
+@application.route("/apply") # apply
+def apply():
+    return render_template("apply.html")
+
+@application.route("/list") # list
+def list_items(): # list라는 함수 이름은 파이선 기본 메서드와 겹치니 list_items로 변경 
+    return render_template("list.html")
+
+
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
